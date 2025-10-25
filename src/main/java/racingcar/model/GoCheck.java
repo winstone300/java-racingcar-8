eb745d6e9112fef96bc43f383ddcb0b55ec049ed
winstone300 <winstone300@navere.com>
2025-10-25 16:16:17 +0900

    test 커밋

---
+++ src/main/java/racingcar/model/GoCheck.java
@@ -0,0 +1,7 @@
+package racingcar.model;
+
+public class GoCheck {
+    public static boolean goCheck(int num) {
+        return num >= 4;
+    }
+}
---
+++ src/main/java/racingcar/model/WinnerCheck.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+
+public class WinnerCheck {
+    public static ArrayList<String> check(String[] names, int[] scores) {
+        ArrayList<String> winners = new ArrayList<>();
+        return winners;
+    }
+}
