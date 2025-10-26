52d6505cfee9243b221861587d72461ae8a7e97f
winstone300 <winstone300@naver.com>
2025-10-26 18:41:44 +0900

    feat(OutputView): 출력 메시지 구현
    Closes #9
    
    Closes #10
    
    Closes #11

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,27 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.model.Car;
+
 public class OutputView {
+    public void printStartPrompt() {
+        System.out.println("실행결과");
+    }
+
+    public void printRoundResult(List<Car> carList) {
+        for (Car car : carList) {
+            String bar = "-".repeat(car.getPos());
+            System.out.println(car.getName() + " : " + bar);
+        }
+        System.out.println();
+    }
+
+    public void printWinnerResult(List<Car> carList) {
+        String s = carList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + s);
+    }
 }
