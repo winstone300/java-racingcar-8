b163131b50bdc88cbfa9e5a1aa1e09b7fee5c87f
winstone300 <winstone300@naver.com>
2025-10-26 18:42:38 +0900

    feat(WinnerCheck): 우승자 확인 기능 구현
    Closes #6

--- src/main/java/racingcar/model/WinnerCheck.java
+++ src/main/java/racingcar/model/WinnerCheck.java
@@ -1,10 +1,17 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinnerCheck {
-    public static ArrayList<String> check(String[] names, int[] scores) {
-        ArrayList<String> winners = new ArrayList<>();
-        return winners;
+    public static List<Car> check(List<Car> carList) {
+        int maxPos = carList.stream()
+                .mapToInt(Car::getPos)
+                .max().orElse(0);
+
+        return carList.stream()
+                .filter(c -> c.getPos()==maxPos)
+                .collect(Collectors.toList());
     }
 }
