6138ddf8afed028ac48f0bca28bb808607ffbe9e
winstone300 <winstone300@naver.com>
2025-10-26 11:23:38 +0900

    refactor(RandomNumber): random 함수 수정

--- src/main/java/racingcar/model/RandomNumber.java
+++ src/main/java/racingcar/model/RandomNumber.java
@@ -1,7 +1,9 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumber {
     public static int randomNumber(int min, int max) {
-        return (int) (Math.random() * (max - min + 1) + min);
+        return Randoms.pickNumberInRange(min,max);
     }
 }
