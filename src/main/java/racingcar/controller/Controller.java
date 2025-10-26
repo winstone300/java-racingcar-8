0f2de7c0bad9bbc0f724138fa46f4f31ff371510
winstone300 <winstone300@naver.com>
2025-10-26 18:44:54 +0900

    feat(Controller): 컨트롤러 구현
    Closes #12
    
    Closes #13

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,10 +1,50 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.CarNameParser;
+import racingcar.model.GoCheck;
+import racingcar.model.RandomNumber;
+import racingcar.model.WinnerCheck;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Controller {
-    public void run(){
-        InputView.printCarNamePrompt();
-        InputView.printAttemptCountPrompt();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private String carNamesCsv;
+    private int attemptCount;
+    private final CarNameParser carNameParser = new CarNameParser();
+    private List<Car> carList;
+    private List<Car> winnerList;
+
+    public void run() {
+        //자동차 이름 입력
+        inputView.printCarNamePrompt();
+        carNamesCsv = Console.readLine();
+        carList = carNameParser.sepCarList(carNamesCsv);
+
+        //시도할 횟수
+        inputView.printAttemptCountPrompt();
+        attemptCount = Integer.parseInt(Console.readLine());
+
+        //실행 결과
+        outputView.printStartPrompt();
+        for (int i = 0; i < attemptCount; i++) {
+            step();
+        }
+
+        //우승자 출력
+        winnerList = WinnerCheck.check(carList);
+        outputView.printWinnerResult(winnerList);
+    }
+
+    public void step() {
+        for (Car car : carList) {
+            int num = RandomNumber.randomNumber(0, 9);
+            GoCheck.moveIfPossible(car, num);
+        }
+        outputView.printRoundResult(carList);
     }
 }
