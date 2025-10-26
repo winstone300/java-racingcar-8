c78ddaa8efc358fd975a1a8c966d641a8169d507
winstone300 <winstone300@naver.com>
2025-10-26 18:47:38 +0900

    test(GoCheckTest): 전진 기능 테스트 추가

--- src/test/java/racingcar/model/GoCheckTest.java
+++ src/test/java/racingcar/model/GoCheckTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,13 +10,17 @@ import org.junit.jupiter.api.Test;
 public class GoCheckTest{
     @Test
     public void 값이_4_이상일때_테스트(){
+        Car car = new Car("car",2);
         int num = RandomNumber.randomNumber(4,9);
-        assertTrue(GoCheck.goCheck(num));
+        GoCheck.moveIfPossible(car,num);
+        assertEquals(car.getPos(),3);
     }
 
     @Test
     public void 값이_4_미만일때_테스트(){
+        Car car = new Car("car",2);
         int num = RandomNumber.randomNumber(0,3);
-        assertFalse(GoCheck.goCheck(num));
+        GoCheck.moveIfPossible(car,num);
+        assertEquals(car.getPos(),2);
     }
 }
